c50577e4e344abd220b21d64eca5411b519c25a4
Artem Alexeev <alieksieiev.artem@airslate.com>
2026-01-27 19:13:51 +0100

    add logic for actions buttons and add pagination for recs

--- public/js/app.js
+++ public/js/app.js
@@ -5,9 +5,10 @@ async function loadRecs(page = 1) {
         const response = await fetch(`/api/recs?page=${page}`);
         const data = await response.json();
 
-        currentPage = data.currentPage;
+        page = data.page;
+
         renderUsers(data.data);
-        renderPagination(data.currentPage, data.totalPages);
+        renderPagination(data.page, data.length);
     } catch (error) {
         console.error('Failed to load users', error);
     }
@@ -25,12 +26,12 @@ function renderUsers(recs) {
 
         card.innerHTML = `
             <img src="${rec.avatar}" class="card-avatar">
-            <div class="card-info">${rec.first_name} ${rec.last_name}, ${rec.age}</div>
+            <div class="card-info">(#${rec.user_id})${rec.first_name} ${rec.last_name}, ${rec.age}</div>
             <div class="card-status">${rec.status_label}</div>
             <div class="card-actions">
-                <button class="dislike" data-id="${rec.id}">✖</button>
-                <button class="super" data-id="${rec.id}">★</button>
-                <button class="like" data-id="${rec.id}">❤</button>
+                <button class="dislike" data-user-id="${rec.user_id}">✖</button>
+                <button class="super" data-user-id="${rec.user_id}">★</button>
+                <button class="like" data-user-id="${rec.user_id}">❤</button>
             </div>
         `;
 
@@ -43,20 +44,34 @@ function renderUsers(recs) {
 /* ACTIONS */
 function attachActions() {
     document.querySelectorAll('.like').forEach(btn => {
-        btn.onclick = () => action(btn.dataset.id, 'like');
+        btn.onclick = () => action(btn.dataset.userId, 'like', btn);
     });
 
     document.querySelectorAll('.dislike').forEach(btn => {
-        btn.onclick = () => action(btn.dataset.id, 'dislike');
+        btn.onclick = () => action(btn.dataset.userId, 'dislike', btn);
     });
 
     document.querySelectorAll('.super').forEach(btn => {
-        btn.onclick = () => action(btn.dataset.id, 'super');
+        btn.onclick = () => action(btn.dataset.userId, 'super', btn);
     });
 }
 
-function action(userId, type) {
+function action(userId, type, btn) {
+    const cardBlock = btn.closest('div').parentElement;
+    cardBlock.classList.add(type + '-clicked');
+
     console.log(`User ${userId}: ${type}`);
+
+    const response = fetch('/api/reactions', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            target_user_id: Number(userId),
+            type: type
+        })
+      });
 }
 
 /* PAGINATION */
@@ -70,19 +85,9 @@ function renderPagination(current, total) {
     prev.onclick = () => loadRecs(current - 1);
     container.appendChild(prev);
 
-    for (let i = current - 1; i <= current + 1; i++) {
-        if (i < 1 || i > total) continue;
-
-        const btn = document.createElement('button');
-        btn.textContent = i;
-        btn.className = 'page' + (i === current ? ' active' : '');
-        btn.onclick = () => loadRecs(i);
-        container.appendChild(btn);
-    }
-
     const next = document.createElement('button');
     next.textContent = 'Next';
-    next.disabled = current === total;
+    next.disabled = total === 0;
     next.onclick = () => loadRecs(current + 1);
     container.appendChild(next);
 }
